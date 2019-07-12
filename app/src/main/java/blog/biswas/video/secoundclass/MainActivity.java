563ca0288c76c427e8b0f3118825829bfacbc9b2
sushen <sushenbiswasaga@gmail.com>
2019-07-12 17:47:15 +0600

    Runtime Polymerphysm.

--- app/src/main/java/blog/biswas/video/secoundclass/MainActivity.java
+++ app/src/main/java/blog/biswas/video/secoundclass/MainActivity.java
@@ -3,12 +3,42 @@ package blog.biswas.video.secoundclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvNumber;
+    Button btn1;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNumber = findViewById(R.id.tvNumber);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.btn1){
+            tvNumber.setText("1");
+        }else if (v.getId() == R.id.btn2){
+            tvNumber.setText("2");
+        }else if(v.getId() == R.id.btn3){
+            tvNumber.setText("3");
+        }
     }
 }
